c11e866cad914c82b74b6798f163bb9b8302f6e6
Angel Negron <negron.a@ufl.edu>
2020-06-05 21:04:14 -0400

    add section d of homework created in javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+//create pizza obect and properties
+var pizza = {
+    crust: "thin",
+    cheese: "cheddar",
+    sauce: "delicious tomato",
+
+    addToppings: function() {
+        console.log('Please add pepperoni and sausage');
+    }
+};
+
+//call the addtoppings method and function
+pizza.addToppings();
